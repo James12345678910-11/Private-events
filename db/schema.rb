92ecd5b3657f33a9801a24a0c5f6db589da42ffd
James Calderon <jamescalderon122@gmail.com>
2026-02-21 18:51:22 +0800

    3rd commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_21_082001) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_21_104002) do
+  create_table "events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "creator_id"
+    t.datetime "date"
+    t.text "description"
+    t.string "location"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -22,4 +33,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_21_082001) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
