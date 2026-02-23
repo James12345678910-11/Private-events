264cc55bbc4aa1b153a0b84288833dc925b5f460
James Calderon <jamescalderon122@gmail.com>
2026-02-23 17:25:14 +0800

    4th commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+        @created_events = @user.created_events
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to @user, notice: "User was successfully created."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
 end
